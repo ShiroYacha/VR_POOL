4817c95967a42b7cf2f6fb6399a081d0e925774a
ShiroYacha <sebzhaomin@gmail.com>
2015-04-12 17:34:15 +0200

    Add simple billiard scene and test physics.

---
+++ Assets/BilliardBallTestPhysics.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BilliardBallTestPhysics : MonoBehaviour {
+	
+	Vector3 initForce = new Vector3(1.0f,0.0f,1.0f)*5;
+	Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+
+		rigidBody = GetComponent<Rigidbody> ();
+		rigidBody.AddForce (initForce,ForceMode.Impulse);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (rigidBody.velocity.magnitude > 0.05f) {
+			rigidBody.AddForce (rigidBody.velocity * (-1) * 0.05f, ForceMode.Impulse);
+		} else {
+			rigidBody.Sleep();
+		}
+	}
+
+}
