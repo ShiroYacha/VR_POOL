377aaa6f08aa999851721748539acc19097e304c
ShiroYacha <sebzhaomin@gmail.com>
2015-04-13 17:12:17 +0200

    Implement basic holes.

--- Assets/BilliardBallTestPhysics.cs
+++ Assets/BilliardBallTestPhysics.cs
@@ -1,24 +1,49 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BilliardBallTestPhysics : MonoBehaviour {
-	
-	Vector3 initForce = new Vector3(1.0f,0.0f,1.0f)*5;
+public class BilliardBallTestPhysics : MonoBehaviour
+{
+
+	Vector3 initPosition = new Vector3 (0.0f, 0.055f, 0.0f);
+	float friction = 0.05f;
+	float maxForce = 40.0f;
+	float minForce = 20.0f;
+
 	Rigidbody rigidBody;
 
 	// Use this for initialization
-	void Start () {
-
+	void Start ()
+	{
 		rigidBody = GetComponent<Rigidbody> ();
-		rigidBody.AddForce (initForce,ForceMode.Impulse);
+		ResetBall ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		if (rigidBody.velocity.magnitude > 0.05f) {
-			rigidBody.AddForce (rigidBody.velocity * (-1) * 0.05f, ForceMode.Impulse);
+	void Update ()
+	{
+		if (rigidBody.velocity.magnitude > friction) {
+			rigidBody.AddForce (rigidBody.velocity * (-1) * friction, ForceMode.Impulse);
 		} else {
-			rigidBody.Sleep();
+			ResetBall();
+		}
+	}
+
+	void ApplyRandomForce()
+	{
+		float force = (maxForce - minForce) * Random.value + minForce;
+		rigidBody.AddForce (new Vector3(Random.value,0.0f,Random.value)*force, ForceMode.Impulse);
+	}
+
+	void ResetBall()
+	{
+		this.transform.position = initPosition;
+		ApplyRandomForce ();
+	}
+
+	void OnCollisionEnter (Collision col)
+	{
+		if (col.gameObject.name.StartsWith ("Hole")) {
+			ResetBall();
 		}
 	}
 
