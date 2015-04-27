84aee9a9d7d95d5396d6def9766bf35da3ce5e16
PatriceGueniat <patrice.gueniat@epfl.ch>
2015-04-27 11:15:20 +0200

    leap version 1

---
+++ Assets/BilliardCue_LeapCommand.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class BilliardCue_LeapCommand : MonoBehaviour {
+
+	GameObject controller;
+	bool closedHand = false;
+	bool openedHand = false;
+	// Use this for initialization
+	void Start () {
+		BilliardCue_Control.OnRotateCue += RotateCueWithWheel;
+		BilliardCue_Control.OnShootingCue += ShootCueWithLeftMouse;
+		BilliardCue_Control.OnReleaseCue += ReleaseCueWithLeftMouse;
+		controller = GameObject.Find ("HandController");
+	}
+	
+	/// </summary>
+	/// <returns>The direction of the rotation (between -1 and 1).</returns>
+	float RotateCueWithWheel()
+	{
+		LeapCommand leap  = controller.GetComponent<LeapCommand> ();
+		Vector pos = leap.getPosition ();
+		int handcount = leap.getHandCount ();
+		float strength = leap.getStrength ();
+		float wheel = 0.0f;
+		float dist = 0.0f;
+		int limit = 50;
+		int sensitivity = 800;
+		if (handcount > 0 && strength != 1) {
+			if (pos.x > limit) {
+				dist = (pos.x - limit) / sensitivity;
+				wheel = dist;
+			}
+			if (pos.x < -limit) {
+				dist = -(pos.x + limit) / sensitivity;
+				wheel = -dist;
+			}
+		}
+		return wheel * 0.5f;// 0.5 improve the precision
+	}
+	
+	/// <summary>
+	/// Shoots the cue with left mouse.
+	/// </summary>
+	/// <returns><c>true</c>, if cue with mouse is shooting, <c>false</c> otherwise.</returns>
+	bool ShootCueWithLeftMouse()
+	{
+		LeapCommand leap  = controller.GetComponent<LeapCommand> ();
+		int handcount = leap.getHandCount ();
+		float strength = 0.0f;
+		if (handcount > 0) {
+			strength = leap.getStrength ();
+			if (strength == 0)
+				openedHand = true;
+		} else {
+			openedHand = false;
+		}
+		if (strength == 1.0f && openedHand) {
+			closedHand = true;
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	/// <summary>
+	/// Releases the cue with left mouse.
+	/// </summary>
+	/// <returns><c>true</c>, if cue with left mouse was released, <c>false</c> otherwise.</returns>
+	bool ReleaseCueWithLeftMouse()
+	{
+		LeapCommand leap  = controller.GetComponent<LeapCommand> ();
+		int handcount = leap.getHandCount ();
+		float strength = 0.0f;
+		if (handcount > 0) {
+			strength = leap.getStrength ();
+		}
+		if (closedHand && strength == 0.0f) {
+			closedHand = false;
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
---
+++ Assets/LeapCommand.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class LeapCommand : MonoBehaviour {
+
+	private float strength = 0.0f;
+	private int handCount = 0;
+	private Vector hand_position;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		HandController controller = gameObject.GetComponent<HandController> ();
+		Frame frame = controller.GetFrame(); // controller is a Controller object
+		HandList hands = frame.Hands;
+		handCount = hands.Count;
+		if (handCount != 0) {
+			Hand hand = hands [0];
+			strength = hand.GrabStrength;
+			hand_position = hand.PalmPosition;
+		}
+	}
+
+	public Vector getPosition(){
+		return hand_position;
+	}
+
+	public int getHandCount(){
+		return handCount;
+	}
+
+	public float getStrength(){
+		return strength;
+	}
+
+	void OnGUI()
+	{
+		GUI.Label (new Rect (0, 0, 100, 100), strength.ToString());
+		GUI.Label (new Rect (0, 100, 100, 100), hand_position.ToString());
+	}
+}
