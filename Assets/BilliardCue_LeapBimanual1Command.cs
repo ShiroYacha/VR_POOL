e1d940250ac033e91c43da81fee2aefe71b4972b
shiroyacha <sebzhaomin@gmail.com>
2015-05-06 11:07:58 +0200

    Add a new bimanual gesture (separate).

---
+++ Assets/BilliardCue_LeapBimanual1Command.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class BilliardCue_LeapBimanual1Command : MonoBehaviour
+{
+
+	GameObject controller;
+	bool closedHand = false;
+	bool openedHand = false;
+	bool startShooting = false;
+	float dist = 0.0f;
+	float DIST_MAX = 0.015f;
+	float DIST_MIN = 0.07f;
+
+	// Use this for initialization
+	void Start ()
+	{
+		BilliardCue_Control.OnRotateCue += RotateCueWithRelativePosition;
+		BilliardCue_Control.OnShootingCue += ShootCueWithHandGrab;
+		BilliardCue_Control.OnReleaseCue += ReleaseCueWithHandGrab;
+		controller = GameObject.Find ("HandController");
+	}
+	
+	/// </summary>
+	/// <returns>The direction of the rotation (between -1 and 1).</returns>
+	float RotateCueWithRelativePosition ()
+	{
+		LeapCommandBimanual leap = controller.GetComponent<LeapCommandBimanual> ();
+		float wheel = 0.0f;
+		if (leap.getValidGesture ()) {
+			Vector leftPos = leap.getLeftHandPosition ();
+			Vector rightPos = leap.getRightHandPosition ();
+			bool bothOpen = leap.getBothOpen ();
+			dist = 0.0f;
+			int limit = 70;
+			int sensitivity = 2500;
+			if (bothOpen) {
+				float left2rightZOffset = leftPos.z - rightPos.z;
+				if(Mathf.Abs(left2rightZOffset)>limit){
+					dist = (leftPos - rightPos).Magnitude / sensitivity;
+					// normalize dist
+					dist = (dist-DIST_MIN)/(DIST_MAX-DIST_MIN)*10.0f;
+					dist = dist*dist/100.0f;
+					// rescale
+					dist *= 0.05f;
+					wheel = Mathf.Sign (left2rightZOffset) * dist ;
+				}
+			}
+		}
+		return wheel * 0.5f;// 0.5 improve the precision
+	}
+
+	void OnGUI()
+	{
+		GUI.Label (new Rect (15, 100, 100, 100), "dist = " +dist.ToString());
+	}
+	
+	/// <summary>
+	/// Shoots the cue with left mouse.
+	/// </summary>
+	/// <returns><c>true</c>, if cue with mouse is shooting, <c>false</c> otherwise.</returns>
+	bool ShootCueWithHandGrab ()
+	{
+		LeapCommandBimanual leap = controller.GetComponent<LeapCommandBimanual> ();
+		if (leap.getValidGesture() && leap.getBothClose ()) {
+			startShooting = true;
+			return true;
+		}
+		return false;
+	}
+	
+	/// <summary>
+	/// Releases the cue with left mouse.
+	/// </summary>
+	/// <returns><c>true</c>, if cue with left mouse was released, <c>false</c> otherwise.</returns>
+	bool ReleaseCueWithHandGrab ()
+	{
+		LeapCommandBimanual leap = controller.GetComponent<LeapCommandBimanual> ();
+		if (startShooting && leap.getValidGesture() && leap.getBothOpen()) {
+			startShooting = false;
+			return true;
+		}
+		return false;
+	}
+}
---
+++ Assets/LeapCommandBimanual.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class LeapCommandBimanual : MonoBehaviour {
+
+	private float strength = 0.0f;
+	private int handCount = 0;
+	private Vector leftHand_position;
+	private Vector rightHand_position;
+	private bool valid_gesture;
+	private bool both_open;
+	private bool both_close;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		HandController controller = gameObject.GetComponent<HandController> ();
+		Frame frame = controller.GetFrame(); // controller is a Controller object
+		HandList hands = frame.Hands;
+		handCount = hands.Count;
+		valid_gesture=false;
+		if (handCount == 2) {
+			Hand hand1 = hands [0];
+			Hand hand2 = hands [1];
+			if(hand1.IsLeft == hand2.IsLeft) return;
+			leftHand_position = hand1.IsLeft?hand1.PalmPosition:hand2.PalmPosition;
+			rightHand_position = hand2.IsRight?hand2.PalmPosition:hand1.PalmPosition;
+			both_open = hand1.GrabStrength==0 && hand2.GrabStrength==0;
+			both_close = hand1.GrabStrength>0 && hand2.GrabStrength>0;
+			valid_gesture = true;
+		}
+	}
+
+	public bool getValidGesture(){
+		return valid_gesture;
+	}
+
+	public Vector getLeftHandPosition(){
+		return leftHand_position;
+	}
+
+	public Vector getRightHandPosition(){
+		return rightHand_position;
+	}
+
+
+	public int getHandCount(){
+		return handCount;
+	}
+
+	public bool getBothOpen(){
+		return both_open;
+	}
+
+	public bool getBothClose(){
+		return both_close;
+	}
+
+	void OnGUI()
+	{
+
+	}
+}
